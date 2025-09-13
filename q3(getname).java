f90fdde5252382c46fa9dbebf8509968c82d5530
deeprajput-codr <codeindeepc@gmail.com>
2025-09-13 10:48:55 +0530

    Add files via upload

---
+++ q3(getname).java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+//user info
+public class q1(getname) {
+    public static void main(String[] args) {
+       System.out.println(" Get user name");
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter your name : ");
+       String name = sc.nextLine();
+       System.out.println("Hello "  + name + " have a great day");
+
+    }
+}
